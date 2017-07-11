7657ee5a14181982c6a175b8e330e8b36c832393
tgxh2020 <tgxh2020@gmail.com>
2017-07-11 22:26:53 +0800

    root切换

--- src/components/Root.vue
+++ src/components/Root.vue
@@ -12,7 +12,7 @@
               'icon-shejiao': i === 2,
               'active': i === cur
             }"
-           @click="cur = i"
+           @click="switchTab(i)"
            v-for="(n,i) in 3"></i>
       </h2>
       <md-button class="md-icon-button">
@@ -21,7 +21,11 @@
         </router-link>
       </md-button>
     </md-toolbar>
-    <router-view></router-view>
+    <transition :name="transitionName">
+      <keep-alive>
+        <router-view class="root-child-view"></router-view>
+      </keep-alive>
+    </transition>
   </div>
 </template>
 
@@ -32,7 +36,8 @@
   export default {
     data() {
       return {
-        cur: 1
+        cur: 1,
+        transitionName: ''
       }
     },
     computed: {
@@ -44,11 +49,27 @@
       })
     },
     methods: {
-      toHome() {
-        this.$router.push({path: '/root/home'})
+      switchTab(index) {
+        this.cur = index
+        switch (index) {
+          case 0:
+            this.$router.push({path: '/root/home'})
+            break
+          case 1:
+            this.$router.push({path: '/root/index'})
+            break
+        }
       }
     },
     filters: {},
+    watch: {
+      $route(to,from) {
+        const arr = ['home','index','social']
+        const toIndex = arr.indexOf(to.path.split('/')[3])
+        const fromIndex = arr.indexOf(from.path.split('/')[3])
+        this.transitionName = toIndex < fromIndex ? 'root-slide-right':'root-slide-left'
+      }
+    }
   }
 </script>
 
@@ -70,21 +91,50 @@
       .iconfont {
         color: #e58983;
         &.active {
-          color:#fff;
+          color: #fff;
         }
         &:not(:last-of-type) {
-          margin-right:pr(15);
+          margin-right: pr(15);
         }
         &:nth-of-type(1) {
-          font-size:pr(20);
+          font-size: pr(20);
         }
         &:nth-of-type(2) {
-          font-size:pr(26);
+          font-size: pr(26);
         }
         &:nth-of-type(3) {
-          font-size:pr(24);
+          font-size: pr(24);
         }
       }
     }
+  }
+
+  .root-child-view {
+    transition: all .3s cubic-bezier(.55, 0, .1, 1);
+    overflow: auto;
+  }
+
+  .root-slide-left-enter, .root-slide-right-leave-active {
+    opacity: 0;
+    -webkit-transform: translate(7.5rem, 0);
+    transform: translate(7.5rem, 0);
+    transition-delay: .3s;
+    -webkit-transition-delay: .3s;
+  }
+
+  .root-slide-left-leave-active, .root-slide-right-enter {
+    opacity: 0;
+    -webkit-transform: translate(-7.5rem, 0);
+    transform: translate(-7.5rem, 0);
+    transition-delay: .3s;
+    -webkit-transition-delay: .3s;
+  }
+  .root-slide-enter-active {
+    -webkit-transition: all .3s ease;
+    transition: all .3s ease;
+  }
+  .root-slide-leave-active {
+    -webkit-transition: all .3s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+    transition: all .3s cubic-bezier(1.0, 0.5, 0.8, 1.0);
   }
 </style>
